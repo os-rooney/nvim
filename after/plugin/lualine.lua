a96c49202eef6879bc8a78bdecec33c76e9b5a68
roosman <dev@ronios.de>
2023-06-15 22:40:29 +0200

    change theme and add pomodoro extension to lua line

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = 'jellybeans',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
@@ -21,16 +21,12 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_w = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'progress', 'location', 'selectioncount', 'searchcount'},
+    lualine_y = {'filesize'},
+    lualine_z = {require('pomodoro').statusline, 'datetime'}
   },
   inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
