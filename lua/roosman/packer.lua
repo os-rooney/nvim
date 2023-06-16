a50879ab4ab7321fcc2065a7505e1657eadc526f
roosman <dev@ronios.de>
2023-06-16 13:41:12 +0200

    add barbar plugin for neovim tabs

--- lua/roosman/packer.lua
+++ lua/roosman/packer.lua
@@ -56,4 +56,10 @@ return require('packer').startup(function(use)
   }
 
   use 'm4xshen/autoclose.nvim'
+
+  -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
+  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+  use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+  use 'romgrk/barbar.nvim'
+
 end)
